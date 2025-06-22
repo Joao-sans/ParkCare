62e0ff9f863dcaad0c7d9428629daec79609d8d0
Joao-sans <joaopedro123.jpss@gmail.com>
2025-06-21 23:20:17 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const elements = document.querySelectorAll('.highlight-hover');
-  
-    elements.forEach(el => {
-      const text = el.getAttribute('data-animated-text');
-      if (!text) return;
-  
-      el.innerHTML = ''; // Limpa o conteúdo atual
-  
-      text.split('').forEach((char, index) => {
-        const span = document.createElement('span');
-        span.textContent = char;
-        span.style.transitionDelay = `${index * 30}ms`; // animação em cascata
-        el.appendChild(span);
-      });
+  const elements = document.querySelectorAll('.highlight-hover');
+
+  elements.forEach(el => {
+    const text = el.getAttribute('data-animated-text');
+    if (!text) return;
+
+    el.innerHTML = '';
+
+    text.split('').forEach((char, index) => {
+      const span = document.createElement('span');
+      span.textContent = char;
+      span.style.transitionDelay = `${index * 30}ms`;
+      el.appendChild(span);
     });
   });
+
+  const menuToggle = document.getElementById("menu-toggle");
+  const menuLinks = document.querySelector(".menu-links");
+
+  if(menuToggle && menuLinks) {
+    menuToggle.addEventListener("click", () => {
+      menuLinks.classList.toggle("show");
+    });
+  }
+});
   
